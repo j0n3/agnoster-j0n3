e708d02d9a85594726af901a8e627f951fadf469
Jose Angel Sanchez (j0n3) <joseangel.sanchez@scmspain.com>
2017-01-02 13:34:03 +0100

    added installer

---
+++ install.zsh
@@ -0,0 +1,27 @@
+#!/bin/zsh
+
+theme_name=agnoster-j0n3
+theme_file=$theme_name.zsh-theme
+oh_my_zsh_themes_folder=${HOME}/.oh-my-zsh/themes
+
+if [ ! -d $oh_my_zsh_themes_folder ]; then
+    echo "ERROR: $oh_my_zsh_themes_folder not found.";
+    exit 1;
+fi
+
+cp $theme_file $oh_my_zsh_themes_folder
+
+if [ -f $oh_my_zsh_themes_folder/$theme_file ]; then
+    echo "Installed theme"
+else
+    echo "ERROR: Couldn't copy to $oh_my_zsh_themes_folder"
+fi
+
+sed -i 's/ZSH_THEME=".*"/ZSH_THEME="'$theme_name'"/' ${HOME}/.zshrc
+
+if grep -Fxq 'ZSH_THEME="'$theme_name'"' ${HOME}/.zshrc ; then 
+    echo "Theme set on .zshrc"
+    source "${HOME}/.zshrc"
+else
+    echo "ERROR: Theme not set on .zshrc"
+fi
